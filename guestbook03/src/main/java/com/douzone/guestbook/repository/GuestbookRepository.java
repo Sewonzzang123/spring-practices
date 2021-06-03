92b03146b08f142cb36cfac2a3a30f88b4a2f3d2
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-06-03 15:12:43 +0900

    guestbook03

---
+++ guestbook03/src/main/java/com/douzone/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,198 @@
+package com.douzone.guestbook.repository;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.douzone.guestbook.vo.GuestbookVo;
+
+@Repository
+public class GuestbookRepository {
+	private Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://192.168.80.112:3307/webdb?characterEncoding=utf8";
+			String id = "webdb";
+			String password = "webdb";
+			conn = DriverManager.getConnection(url, id, password);
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error" + e);
+		}
+
+		return conn;
+	}
+
+	public boolean insert(GuestbookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+
+		try {
+			conn = getConnection();
+
+			String sql = "insert " + "into guestbook " + "values(null,?,?,?,sysdate()) ";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getMessage());
+
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+	}
+
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> result = new ArrayList<>();
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = "select no, name, password, message, reg_date " + "from guestbook "
+					+ "order by reg_date desc ";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String password = rs.getString(3);
+				String message = rs.getString(4);
+				String regDate = rs.getString(5);
+
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setPassword(password);
+				vo.setMessage(message);
+				vo.setRegDate(regDate);
+
+				result.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+	}
+
+	public boolean delete(GuestbookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+
+		try {
+			conn = getConnection();
+
+			String sql = "delete from guestbook " + " where no=? and password=? ";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setLong(1, vo.getNo());
+			pstmt.setString(2, vo.getPassword());
+
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+
+	}
+
+	public int countAll() {
+
+		int result = 0;
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = "select count(*) " 
+					+ "from guestbook ";
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				result = rs.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+	}
+}
