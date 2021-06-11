7189597c899bf755f140d0d64bc1e5d4b68c469b
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-06-11 15:25:59 +0900

    wiring

--- container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
+++ container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
@@ -2,20 +2,29 @@ package com.douzone.container.user.test;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+import com.douzone.container.user.User;
 import com.douzone.container.user.User1;
 
 public class XmlConfigTest {
 
 	public static void main(String[] args) {
 		// XML 의 Auto Configuration(Annotation Scanning)
-		//testBeanFactory01();
+		// testBeanFactory01();
 
 		// XML Bean Configuration(Explicit Configuration)명시적으로 설정한다..
-		testBeanFactory02();
-	}
+		// testBeanFactory02();
 
+		// XML 의 Auto Configuration(Annotation Scanning)
+		// testApplicationContext01();
+
+		// XML Bean Configuration(Explicit Configuration)명시적으로 설정한다..
+		testApplicationContext02();
+	}
 
 	// XML 의 Auto Configuration(Annotation Scanning)
 	private static void testBeanFactory01() {
@@ -30,10 +39,78 @@ public class XmlConfigTest {
 		System.out.println(user1.getName());
 
 	}
-	
 
+	// XML Bean Configuration(Explicit Configuration)명시적으로 설정한다..
 	private static void testBeanFactory02() {
-		// TODO Auto-generated method stub
+		BeanFactory bf = new XmlBeanFactory(
+				new ClassPathResource("com/douzone/container/config/user/applicationContext02.xml"));
+
+		User1 user1 = bf.getBean(User1.class);
+		System.out.println(user1.getName());
+
+		// Bean id 가 자동으로 설정되지 않음
+		// 명시적으로 설정해야됨
+		user1 = (User1) bf.getBean("user1");
+		System.out.println(user1.getName());
+
+	}
+
+	private static void testApplicationContext01() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"com/douzone/container/config/user/applicationContext01.xml");
+
+		User1 user1 = ac.getBean(User1.class);
+		System.out.println(user1.getName());
+
+		// Bean id 가 자동으로 설정됨
+		user1 = (User1) ac.getBean("user1");
+		System.out.println(user1.getName());
+
+		// 자원 정리
+		((ConfigurableApplicationContext) ac).close();
+
+	}
+
+	// XML Bean Configuration(Explicit Configuration)명시적으로 설정한다..
+	private static void testApplicationContext02() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"com/douzone/container/config/user/applicationContext02.xml");
+
+		// id로 빈 가져오기
+		User user = (User) ac.getBean("user");
+		System.out.println(user);
+
+		// name으로 빈 가져오기
+		user = (User) ac.getBean("usr");
+		System.out.println(user);
+
+		// type으로 가져오기
+		// 같은 타입의 빈이 2개 이상 있는 경우 type으로 가져오면 error
+		// 1. type+id
+		// 2. ....
+		user = ac.getBean("user2", User.class);
+		System.out.println(user);
+
+		user = ac.getBean("user3", User.class);
+		System.out.println(user);
+
+		user = ac.getBean("user4", User.class);
+		System.out.println(user);
+
+		// setter를 사용한 빈 가져오기1
+		user = ac.getBean("user5", User.class);
+		System.out.println(user);
+
+		// setter를 사용한 빈 가져오기2 (DI)
+		user = ac.getBean("user6", User.class);
+		System.out.println(user);
 		
+		// setter를 사용한 빈 가져오기3 (List 프로퍼티)
+		user = ac.getBean("user7", User.class);
+		System.out.println(user);
+
+		// 자원 정리
+		((ConfigurableApplicationContext) ac).close();
+
 	}
 }
